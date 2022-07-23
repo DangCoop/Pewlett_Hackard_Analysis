a953a03bbbbd886ec87d5713ef6996128dc53796
Denis Antonov <antonov.reca@gmail.com>
2022-07-22 21:44:09 -0400

    Add new file with 7th table

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE departments (
 CREATE TABLE employees (
       emp_no INT NOT NULL,
       birth_date DATE NOT NULL,
-      first_name VARCHAR(10) NOT NULL,
+      first_name VARCHAR(20) NOT NULL,
       last_name VARCHAR(20) NOT NULL,
       gender VARCHAR(20) NOT NULL,
       hire_date DATE NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE employees (
 
 -- Creating table for Managers 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -58,3 +58,44 @@ CREATE TABLE titles(
      FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
      PRIMARY KEY (emp_no)
 );	
+-- Select employees in specific period
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+--Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Count of employees
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Create a new table with the nessesary date
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM  retirement_info
